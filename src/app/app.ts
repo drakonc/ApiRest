ad8be913442981075553d91569d54ac30d9de4dd
Jose Castro <drakonc@gmail.com>
2019-08-04 21:35:00 -0500

    Creaando Variable Port

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,37 +4,39 @@ import cors from 'cors';
 
 //Importacion de las Rutas
 import indexRouter from './routes/index.routes';
-import tsolicitudRouter from './routes/tsolicitud.routes'
+import tsolicitudRouter from './routes/tsolicitud.routes';
 
 export class App {
-  private app: Application;
+	private app: Application;
+	private port?: number | string;
 
-  constructor(private port?: number | string) {
-    this.app = express();
-    this.seting();
-    this.middleware();
-    this.routers();
-  }
+	constructor(port?: number | string) {
+		this.port = port;
+		this.app = express();
+		this.seting();
+		this.middleware();
+		this.routers();
+	}
 
-  seting(): void {
-    this.app.set('port', this.port || process.env.PORT || 3000);
-  }
+	seting(): void {
+		this.app.set('port', this.port || process.env.PORT || 3000);
+	}
 
-  middleware(): void {
-    this.app.use(morgan('dev'));
-    this.app.use(cors());
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: false }));
-  }
+	middleware(): void {
+		this.app.use(morgan('dev'));
+		this.app.use(cors());
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: false }));
+	}
 
-  routers(): void {
-    this.app.use('/', indexRouter);
-    this.app.use('/api', tsolicitudRouter);
-  }
+	routers(): void {
+		this.app.use('/', indexRouter);
+		this.app.use('/api', tsolicitudRouter);
+	}
 
-  async listen() {
-    await this.app.listen(this.app.get('port'), () => {
-      console.log(`Servidor Corriendo en el Puerto ${this.app.get('port')}`);
-    });
-  }
+	async listen() {
+		await this.app.listen(this.app.get('port'), () => {
+			console.log(`Servidor Corriendo en el Puerto ${this.app.get('port')}`);
+		});
+	}
 }
