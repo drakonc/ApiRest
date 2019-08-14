fd3975dadc539df4c7c3bd482049fd5a14a77889
Jose Castro <drakonc@gmail.com>
2019-08-14 13:36:20 -0500

    Iniciando La Api para el Area de Infraestructura

--- src/app/config/keys.ts
+++ src/app/config/keys.ts
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: "bmuvwlqbsdnwf91foafz-mysql.services.clever-cloud.com",
-        user: "uymrwv2qyw7omw60",
-        password: "w16echtiZnqHkx43ls8j",
-        database: "bmuvwlqbsdnwf91foafz"
+        host: "localhost",
+        user: "admin",
+        password: "Passw0rd!!",
+        database: "app"
     }
 };
